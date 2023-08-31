8ba81b9079e6fca358f1bc36b15dbc166ee9c224
Abdullah Alqahtani <0xqahta0@gmail.com>
2023-08-31 21:13:17 +0300

    feat: fetch & store the latest movie

--- integrations/tmdb.go
+++ integrations/tmdb.go
@@ -1,5 +1,44 @@
 package integrations
 
-func FetchLatestMovie() {
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
 
+	"github.com/qahta0/movies/models"
+	"gorm.io/gorm"
+)
+
+const (
+	baseMovieEndpoint   = "api.themoviedb.org/3/movie"
+	latestMovieEndpoint = "latest"
+)
+
+func FetchLatestMovie(accessTokenAuth string) (*models.Movie, error) {
+	url := fmt.Sprintf("https://%s/%s", baseMovieEndpoint, latestMovieEndpoint)
+	req, _ := http.NewRequest("GET", url, nil)
+	req.Header.Add("accept", "application/json")
+	req.Header.Add("Authorization", fmt.Sprintf("Bearer %s", accessTokenAuth))
+	res, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer res.Body.Close()
+	movie := &models.Movie{}
+	if err := json.NewDecoder(res.Body).Decode(movie); err != nil {
+		return nil, err
+	}
+	return movie, nil
+}
+
+func StoreLatestMovie(dbConnection *gorm.DB, movie *models.Movie) error {
+	if err := dbConnection.Where("id = ?", movie.ID).First(movie).Error; err == nil {
+		log.Printf("Movie already exists in DB (id: %d title: %s)", movie.ID, movie.Title)
+		return nil
+	}
+	if err := dbConnection.Create(&movie).Error; err != nil {
+		return err
+	}
+	return nil
 }
--- main.go
+++ main.go
@@ -1,13 +1,22 @@
 package main
 
 import (
+	"os"
+
 	"github.com/qahta0/movies/db"
 	"github.com/qahta0/movies/helpers"
+	"github.com/qahta0/movies/integrations"
 )
 
 func main() {
 	helpers.LoadEnv()
 	dbConnection := db.Connect()
 	db.FlushAndMigrate(dbConnection, false)
+	accessTokenAuth := os.Getenv("ACCESS_TOKEN_AUTH")
+	latestMovie, err := integrations.FetchLatestMovie(accessTokenAuth)
+	if err != nil {
+		return
+	}
+	integrations.StoreLatestMovie(dbConnection, latestMovie)
 	select {}
 }
